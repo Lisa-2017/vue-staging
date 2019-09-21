d8322098e649514fe8c554acd01e8a62be750243
Leesa <18817396285@163.com>
2019-09-21 20:00:28 +0800

    规范注释，elintrc文件的rules中添加了几条忽略配置

--- src/main.js
+++ src/main.js
@@ -1,19 +1,17 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+/* 引入vue */
 import Vue from 'vue'
+/* 引入组件 */
 import App from './App.vue'
+/* 控制台提示信息 */
+Vue.config.productTip = false
 
-Vue.config.productionTip = false
-
-// 注释---忽略检查new出来的函数没调用的警告信息
 /* eslint-disable no-new */
 
-// 设置控制提示信息：如果不写，默认值是true
-Vue.config.productionTip = false
-
-// 实例化Vue对象
+// 实例化对象
 new Vue({
-  el: '#app', /* 选择器 */
-  components: { App }, /* 注册组件 */
-  template: '<App/>' /* 模板 */
+  el:'#app',
+  components: {
+    App
+  },
+  template:'<App/>'
 })
